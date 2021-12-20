524844b0ec6122668dbb38863c76ebef6c97e2b8
adrianstando <adrsnek11@live.com>
2021-12-20 23:55:13 +0100

    Mount static html to app

--- python/main.py
+++ python/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+api = FastAPI(openapi_prefix="/api")
+app.mount("/api", api)
+app.mount("/", StaticFiles(directory="html", html=True), name="html")
 
-@app.get("/")
+
+@api.get("/hello")
 def root():
     return {"message": "Hello World"}
